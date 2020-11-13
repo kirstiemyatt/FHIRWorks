d02c228ddef9f934bf0eefdb5bba1b6c8067b07a
yasser.khan4 <yasser.khan4@nhs.net>
2020-11-13 13:35:49 +0000

    removed doc strings and cleaned up main func

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
+
 @app.route('/create_appointment',methods = ['POST'])
 def create_appointment():
     print(request.args)
@@ -41,31 +42,6 @@ def create_appointment():
     return json.dumps(appointment_dict)
 
 
-
-'''
-
-Hit FHIR server and return response
-Obtain response and and trim down to :
-
-{
-  "appointments": [
-    {
-      "status": "booked",
-      "description": "Some description",
-      "start": "2021-02-01T00:00:00+00:00",
-      "end": "2021-02-02T00:00:00+00:00"
-    },
-    {
-      "status": "booked",
-      "description": "Some description",
-      "start": "2021-02-01T00:00:00+00:00",
-      "end": "2021-02-02T00:00:00+00:00"
-    }
-  ]
-}
-'''
-
-
 def return_appointment () :
     endpoint = "https://fhirworks.azurehealthcareapis.com/Appointment?Patient/cfe4ccf9-1b57-4c79-8350-38363c252ac2"
     headers = {'Content-type': 'application/json', "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImtnMkxZczJUMENUaklmajRydDZKSXluZW4zOCIsImtpZCI6ImtnMkxZczJUMENUaklmajRydDZKSXluZW4zOCJ9.eyJhdWQiOiJodHRwczovL2ZoaXJ3b3Jrcy5henVyZWhlYWx0aGNhcmVhcGlzLmNvbSIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0L2E4ZjQ2NDBmLWFmZWUtNDNkNi1iNTk0LTZjMWRiMjE5NmVjYS8iLCJpYXQiOjE2MDUyNzMxMDMsIm5iZiI6MTYwNTI3MzEwMywiZXhwIjoxNjA1Mjc3MDAzLCJhY3IiOiIxIiwiYWlvIjoiQVNRQTIvOFJBQUFBZHd2SERMdEdicVh1dUpKbTNHeUZZRHlWOUh2TUhjeWtWWVNydFlrV0Nlcz0iLCJhbXIiOlsicHdkIl0sImFwcGlkIjoiYWRlMjAyYmYtNTRhMS00MmYyLTlmODItMjFlYmRjZTMxMWY1IiwiYXBwaWRhY3IiOiIxIiwiaXBhZGRyIjoiOTIuNDAuMjAyLjg3IiwibmFtZSI6IkZoaXJ3b3JrcyIsIm9pZCI6IjVhMGJmN2EwLTVmMzItNGM3MC1iZGRkLWM4N2Q3OGU0YTEwZiIsInB1aWQiOiIxMDAzMjAwMEY3QjVDNkUzIiwicmgiOiIwLkFUb0FEMlQwcU82djFrTzFsR3dkc2hsdXlyOEM0cTJoVlBKQ240SWg2OXpqRWZVNkFLcy4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzdWIiOiJmVEdERGx4LVRFdzd2a0FjaV9lMGxiV3RSS1d2dW1FZ3Q1OHZPUmpCS0dRIiwidGlkIjoiYThmNDY0MGYtYWZlZS00M2Q2LWI1OTQtNmMxZGIyMTk2ZWNhIiwidW5pcXVlX25hbWUiOiJmaGlyd29ya3NAYWxleGhhd2Rvbmdvb2dsZW1haWwub25taWNyb3NvZnQuY29tIiwidXBuIjoiZmhpcndvcmtzQGFsZXhoYXdkb25nb29nbGVtYWlsLm9ubWljcm9zb2Z0LmNvbSIsInV0aSI6ImNCOXJYYW05UmtTaUMtNWpFM1FkQWciLCJ2ZXIiOiIxLjAifQ.aY89_BacaH0eXIA-mSmZZT1gPaVVsfDRKpswLFjSPXwei8T19Q5e12ROjA0nP-Uas-6WD6fvCzrHV4vLoC2yHGD67fqc7Zy7UFU1hy6E_8nz-ekvNgHdlr_j6eq7zRCwGh70xOS49RcPTPdNYvh7iisoKE1y7Pw8s6aKt7_H5XlZjIrXlUL5RYLprcHmQT5ZW4l1haT6VwVmD2K3VAejhyxZygk7n9ZmU0j1gza25qlb3FqIRAREb5AKlCIGYLNhbke3TxJQzIn-YdJ3G_fun3-gR3klXVZU5gaVQMr7UGFkfRToK83FzKJobUyZH8ejMirIaH_0cj9jsM9BJhRRXw"}
@@ -80,4 +56,4 @@ def return_appointment () :
 
 
 if __name__ == '__main__':
-  print(return_appointment())
+  app.run()
