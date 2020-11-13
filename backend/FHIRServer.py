7e19ca6ee7e1f457f484d0377079cc96f46c409a
Alex Hawdon <alex.hawdon@airelogic.com>
2020-11-13 14:18:49 +0000

    created FHIRServer to mediate communication with Azure FHIR API

---
+++ backend/FHIRServer.py
@@ -0,0 +1,38 @@
+import requests
+from time import time
+
+CLIENT_ID = "4fd5577c-c9a2-4a7b-9a58-575c264254fa"
+CLIENT_SECRET = "tup3m3ipxV._-.5a3s-ZtRLJnV2702Qg9B"
+BASE_URI = "https://fhirworks.azurehealthcareapis.com"
+TOKEN_ENDPOINT = "https://login.microsoftonline.com/a8f4640f-afee-43d6-b594-6c1db2196eca/oauth2/token"
+
+
+class FHIRServer:
+    def __init__(
+        self, client_id: str, client_secret: str, token_endpoint: str, base_uri: str
+    ):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.token_endpoint = token_endpoint
+        self.base_uri = base_uri
+        self._token_response = None
+
+    def _access_token(self):
+        if (
+            self._token_response is None
+            or int(self._token_response["expires_on"]) <= time.time()
+        ):
+            self._token_response = requests.post(
+                self.token_endpoint,
+                data={
+                    "client_id": self.client_id,
+                    "client_secret": self.client_secret,
+                    "resource": self.base_uri,
+                    "grant_type": "client_credentials",
+                },
+            ).json()
+        return self._token_response['access_token']
+
+    def Patient(self):
+        r = requests.get(f"{self.base_uri}/Patient", headers={"Authorization": f"Bearer {self._access_token()}"})
+        return r.json()
